fd46b4317717e8648f5972bca04709c8d9a37da3
Stephen Maitland <s.maitland@me.com>
2016-07-28 21:55:14 +0100

    Completed version

--- ETimer/ViewController.swift
+++ ETimer/ViewController.swift
@@ -10,9 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let startTime = 524
+    var currentTimeNum = 0
+    var timer = Timer()
+    
+    @IBOutlet weak var currentTime: UILabel!
+    
+    @IBAction func start(_ sender: AnyObject) {
+        startTimer()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pause(_ sender: AnyObject) {
+        timer.invalidate()
+    }
+    
+    @IBAction func plus10(_ sender: AnyObject) {
+        currentTimeNum += 10
+        updateDisplay()
+    }
+    
+    @IBAction func minus10(_ sender: AnyObject) {
+        if currentTimeNum > 10 {
+            currentTimeNum -= 10
+            updateDisplay()
+        }
+    }
+    
+    @IBAction func reset(_ sender: AnyObject) {
+        currentTimeNum = startTime
+        updateDisplay()
+    }
+    
+    func processTimer() {
+        currentTimeNum -= 1
+        if currentTimeNum == 0 {
+            timer.invalidate()
+        }
+        updateDisplay()
+    }
+    
+    func updateDisplay() {
+        currentTime.text = String(currentTimeNum)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentTimeNum = startTime
+        updateDisplay()
+
     }
 
     override func didReceiveMemoryWarning() {
